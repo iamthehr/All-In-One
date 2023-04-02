1aeefdfc0bd79d6a64879a02476e77bec3fce680
Aditya5510 <78204626+Aditya5510@users.noreply.github.com>
2023-04-03 02:44:30 +0530

    added other routes

--- src/pages/Sign_up.jsx
+++ src/pages/Sign_up.jsx
@@ -136,7 +136,7 @@ export default function SignUp() {
             <CustomButton></CustomButton>
             <Grid container justifyContent="flex-end">
               <Grid item>
-                <Link href="#" variant="body2">
+                <Link href="/Login" variant="body2">
                   Already have an account? Sign in
                 </Link>
               </Grid>
