4e9a55864925ab020c95f856d85fd66432b8774b
iamthehr <harshraj5565@gmail.com>
2023-04-16 11:20:00 +0530

    user Profile

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,9 +1,8 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript, title } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-
       <Head>
         <link
           rel="stylesheet"
@@ -11,7 +10,8 @@ export default function Document() {
           integrity="sha256-kLaT2GOSpHechhsozzB+flnD+zUyjE2LlfWPgU04xyI="
           crossorigin=""
         />
-        <link rel="favicon" href='/favicon.png' />
+        <link rel="favicon" href="/favicon.png" />
+        <title>All is One : Solution to all HealthCare Problems</title>
       </Head>
       <body>
         <Main />
